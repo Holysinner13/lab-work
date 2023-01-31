1414be6ba3ed37f4c39561a2499aa28737ba4b34
Anvarov Andrew <dogmanitoz@gmail.com>
2023-01-31 20:35:25 +0600

    Done lab 1 task 6

--- lab work 1/tasks.py
+++ lab work 1/tasks.py
@@ -1,3 +1,7 @@
+import numpy as np
+from random import randint
+
+
 print('Загадай число от 1 до 100.')
 start = 1
 finish = 100
@@ -13,3 +17,28 @@ while True:
     print('Ты загадал число -', number)
     break
 print('Угадал! Молодец!')
+
+
+matrix = np.array([[randint(0, 10) for i in range(3)] for x in range(3)])
+print('Матрица - \n', matrix, '\n')
+
+print('Сумма всех чисел в матрице: ', sum(map(sum, matrix)))
+print('Максимальный элемент в матрице: ', max(map(max, matrix)))
+print('Сумма чисел в первой строке: ', sum(matrix[0]))
+
+result = []
+
+for i in matrix:
+    result.append(i[1])
+
+print('Минимальный элемент во втором столбце: ', min(result))
+
+max_num = 0
+count = 0
+result2 = []
+
+for i in matrix:
+    result2.append(i[count])
+    count += 1
+
+print('Максимальный элемент по главной диагонали', max(result2))

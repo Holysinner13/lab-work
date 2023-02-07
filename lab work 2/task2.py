5a60080a617d0a60bbceb51ab4098483d931cf82
Anvarov Andrew <dogmanitoz@gmail.com>
2023-02-07 20:50:04 +0600

    update

--- lab work 2/task2.py
+++ lab work 2/task2.py
@@ -9,9 +9,9 @@ sleep(2)
 
 def new_power(a, b):
     result = a ** b
-    return print(f'Число {a} в степени {b} = {result}')
+    return f'Число {a} в степени {b} = {result}'
 
 
 nums = input('Введите два целых числа (через пробел): ')
-new_power(int(nums.split()[0]), int(nums.split()[1]))
+print(new_power(int(nums.split()[0]), int(nums.split()[1])))
 print()

68fa345a7dea46a2d38545de04fbb42dff40ea0e
Anvarov Andrew <dogmanitoz@gmail.com>
2023-02-09 20:40:00 +0600

    update

--- lab work 3/task4.py
+++ lab work 3/task4.py
@@ -7,3 +7,11 @@ logger.info('Выполнение задания 4...')
 sleep(2)
 
 
+class MyString(str):
+    """Класс, позволяющий наделять строки методами append() и pop(), выполняющими те же операции, что и у класса list"""
+
+    def str_append(self, stroka):
+        str(result) += stroka
+
+
+        return result

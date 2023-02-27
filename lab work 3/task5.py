225a513af0298bd5459ea96b3289d979fe581b5b
Anvarov Andrew <dogmanitoz@gmail.com>
2023-02-27 22:47:53 +0600

    lab 3 complete task5

--- lab work 3/task5.py
+++ lab work 3/task5.py
@@ -12,14 +12,25 @@ class MyString(str):
     A class, that allows you to endow strings with append() and pop()
     methods that perform the same operations as the list class
     """
+    def __init__(self):
+        self.stroka = ''
 
-    def str_append(self, stroka):
-        string_list = []
-        string_list.append(stroka)
+    @property
+    def get_str(self):
+        return self.stroka
 
-        return ''.join(string_list), string_list
+    @get_str.setter
+    def get_str(self, new_word):
+        self.stroka += new_word + ' '
+
+    def del_str(self):
+        self.stroka = ' '.join(list(self.stroka.split()[:-1]))
 
 
 some_string = MyString()
-print(some_string.str_append('Пиво'))
-print(some_string.str_append('Чипсы'))
+some_string.get_str = 'Пиво'
+some_string.get_str = 'Чипсы'
+some_string.get_str = 'Раки'
+print(some_string.get_str)
+some_string.del_str()
+print(some_string.get_str)

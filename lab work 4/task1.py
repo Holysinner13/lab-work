bddc378271dba18b6480dc5aac36d8948aa6958c
Anvarov Andrew <dogmanitoz@gmail.com>
2023-03-05 19:36:46 +0600

    add empty lab work 4 and 5

---
+++ lab work 4/task1.py
@@ -0,0 +1,49 @@
+from main import logger
+from time import sleep
+import requests
+import easygui
+import random
+from easygui import *
+
+
+"""Задание 1"""
+logger.info('Выполнение задания 1...')
+sleep(2)
+
+
+class Custom:
+    """Класс таможня"""
+
+
+
+
+
+
+
+
+
+
+
+# def get_quote(format, key, lang='ru'):
+#
+#     url = f'https://api.forismatic.com/api/1.0/Quote&key={key}&format={format}&lang={lang}'
+#     response = requests.get(url)
+#     print(response.text)
+#     print(response.status_code)
+#
+#
+# if __name__ == '__main__':
+#     get_quote(format='xml', key=random.randint(100000, 999999))
+
+
+# msg = "Select word"
+# title = "Shop"
+# button = "Ответить"
+# choices = ['Dog', 'Hund', 'Собака']
+#
+# reply = choicebox(msg, title=title, choices=choices)
+#
+# print('You selected: ', end='')
+# print(reply)
+
+
---
+++ lab work 5/task2.py
@@ -0,0 +1,7 @@
+from main import logger
+from time import sleep
+
+
+"""Задание 2"""
+logger.info('Выполнение задания 1...')
+sleep(2)

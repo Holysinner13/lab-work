2fcc7d3611a55c02cab01d300a1e8b7c886be115
Anvarov Andrew <dogmanitoz@gmail.com>
2023-03-26 15:17:09 +0600

    lab work 6 all tasks and distr

---
+++ lab work 6/task2.py
@@ -0,0 +1,9 @@
+from main import logger
+from time import sleep
+
+
+"""Exercise 2"""
+logger.info('Выполнение задания 2...')
+sleep(2)
+
+
---
+++ lab work 6/task3.py
@@ -0,0 +1,9 @@
+from main import logger
+from time import sleep
+
+
+"""Задание 3"""
+logger.info('Выполнение задания 3...')
+sleep(2)
+
+

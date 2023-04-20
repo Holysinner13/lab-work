a40b34afb469a000c285fe59533b270dbe1628d5
Anvarov Andrew <dogmanitoz@gmail.com>
2023-04-20 20:59:37 +0600

    update lab work 5 task 3 (something strange)

--- lab work 5/task3.py
+++ lab work 5/task3.py
@@ -6,6 +6,7 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 
 """Задание 3"""
@@ -13,15 +14,31 @@ logger.info('Выполнение задания 3...')
 sleep(2)
 
 
-def get_film():
+def get_film_info():
+    with open('test.html', encoding="utf-8") as file:
+        src = file.read()
+
+    soup = BeautifulSoup(src, 'lxml')
+    print(soup)
+    # with open('film_info', 'w+', encoding="utf-8") as file:
+    items_divs = soup.find_all('div', class_='shortstory__title')
+    for item in items_divs:
+        item_title = item.find('h2')
+        print(item_title)
+
+
+def get_content():
     driver = webdriver.Chrome(
         service=Service('distr/chromedriver.exe')
     )
-    url = 'https://kinogo.biz/istoricheskie'
-
     try:
+        url = 'https://kinogo.biz/istoricheskie/'
         driver.get(url=url)
         time.sleep(3)
+        find_element1 = driver.find_element(By.ID, 'dle-content')
+        if find_element1:
+            with open('test.html', 'w', encoding="utf-8") as file:
+                file.write(driver.page_source)
     except Exception as _ex:
         print(_ex)
     finally:
@@ -30,7 +47,8 @@ def get_film():
 
 
 def main():
-    get_film()
+    get_content()
+    get_film_info()
 
 
 main()
